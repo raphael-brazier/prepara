c103e1c5a7bc6c7a1f10241a2fdce3220952817e
raphael-brazier <129342917+raphael-brazier@users.noreply.github.com>
2024-06-18 22:33:15 -0300

    Innitial Commit add project
    start project and all archives in project

---
+++ js/banco.js
@@ -0,0 +1,35 @@
+document
+  .getElementById("userForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Impede o envio padrão do formulário
+
+    // Coletar dados do formulário
+    const formData = new FormData(event.target);
+    const data = {
+      nome: formData.get("nome"),
+      email: formData.get("email"),
+      telefone: formData.get("telefone"),
+      endereco: formData.get("endereco"),
+      formacao: formData.get("formacao"),
+      cargo: formData.get("cargo"),
+      objetivos: formData.get("objetivos"),
+    };
+
+    // Enviar dados para a API 'SheetDB'
+    fetch("https://sheetdb.io/api/v1/ohbbko9kl2bdl", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        // Redirecionar o usuário após o envio bem-sucedido
+        window.location.href = "http://127.0.0.1:5500/enviadoSucess.html";
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  });
